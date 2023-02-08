719428ad04dd84c57b937b7d9f806e9b45a13a91
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-02-08 12:17:05 +0000

    Remove rust code for file_to_vector

--- functions.py
+++ functions.py
@@ -10,23 +10,6 @@ def file_to_list(filename: str):
 # use std::fs::File;
 # use std::io::{BufRead, BufReader};
 
-# fn file_to_vector(filename: &str) -> Vec<String> {
-#     let file: File = match File::open(filename) {
-#         Ok(file) => file,
-#         Err(error) => panic!("Couldn't open file: {}", error),
-#     };
-
-#     let reader: BufReader<File> = BufReader::new(file);
-#     let mut words: Vec<String> = Vec::new();
-
-#     for line in reader.lines() {
-#         let word: String = line.unwrap();
-#         words.push(word);
-#     }
-
-#     words
-# }
-
 # pub fn get_first_names() -> Vec<String> {
 #     file_to_vector("first_names.txt")
 # }
