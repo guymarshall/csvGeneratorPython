9068c636b940e9f9f100e8a73b508b5ace76b9a6
Guy Marshall <guymarshall9@gmail.com>
2023-02-10 21:21:01 +0000

    Add curriculum class with init

---
+++ classes/curriculum.py
@@ -0,0 +1,6 @@
+class Curriculum:
+    def __init__(self, id, student_id, subject_id, number_of_lessons_per_week):
+        self.id = id
+        self.student_id = student_id
+        self.subject_id = subject_id
+        self.number_of_lessons_per_week = number_of_lessons_per_week
